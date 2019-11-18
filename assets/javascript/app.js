c113f18b420c2f0c12c757677d27731309de70e2
Sara Bolduc <sara.s.bolduc@gmail.com>
2019-11-17 20:09:27 -0800

    working on database; & adding to html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+    // FIREBASE DATABASE
+    //==============================
 
     // Your web app's Firebase configuration
     var firebaseConfig = {
@@ -14,6 +16,71 @@ $(document).ready(function() {
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
 
+    // VARIABLES
+    //================================
+    //create a variable to reference the database
+    var database = firebase.database();
 
+    //connections directory set up
+    var connectionsRef = database.ref("/players");
+
+    //location in database where client's connection status is stored, auto updates. True if connected, False if not connected.
+    var connectedRef = database.ref(".info/connected");
+
+    //player objects
+    var player1 = null;
+    var player2 = null;
+
+    //player names
+    var player1Name = "";
+    var player2Name = "";
+
+    //player name to be stored on browser
+    var myPlayerName = "";
+
+    //player choices
+    var player1Choice = "";
+    var player2Choice = "";
+
+    //who's turn
+    var turn = 1;
+
+    // FUNCTIONS
+    //================================
+    //when client's connection state changes
+    connectedRef.on("value", function(snap){
+        //does player 1 exixt in database
+        //if connected
+        if (snap.val()){
+            //add user to database
+            var player = connectionsRef.push(true);
+            //remove from database when disconnected
+            player.onDisconnect().remove();
+        }
+
+        //does player 2 exisit in database
+    });
+
+    //when page first loads or connections list changes
+    connectionsRef.on("value", function(snap){
+        var usersOnline = snap.numChildren();
+
+        if (usersOnline == 1){
+            //they are player 1, ask their name & write it to DOM
+            console.log(usersOnline, "players online");
+
+            //message to DOM "waiting for playerr 2"
+        }
+        else if (usersOnline == 2){
+            //they are player 2, ask their name & write it to DOM
+            console.log(usersOnline, "players online");
+
+            //begin game, player 1 goes first
+        }
+        else if (usersOnline > 2){
+            //they cannot play yet...
+            console.log(usersOnline, "players online");
+        }
+    })
     
-}
+});
